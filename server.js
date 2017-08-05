90d4a8756d5a388ed786804d03002dbfb4329356
Daniel Latimer <daniellatimer@gmail.com>
2017-08-05 10:23:06 -0300

    extract first class

--- server.js
+++ server.js
@@ -7,101 +7,25 @@ var moment = require('moment');
 var RSVP = require('rsvp');
 var nodemailer = require('nodemailer');
 var fs = require("fs");
+const { Ad } = require('./classes/ad.js');
 
 var config = require('./config');
 
 let processedAds = loadProcessedAds() || [];
 
-class Ad {
-    constructor(url, image, title, description, location, price, datePosted) {
-        this.url = url;
-        this.image = image;
-        this.title = title;
-        this.description = description;
-        this.location = location;
-        this.price = price;
-        this.isBusiness = null;
-        this.datePosted = datePosted;
-    }
-
-    static buildAd($jquerySelector) {
-        var ad = new Ad();
-
-        ad.url = 'http://www.kijiji.ca' + $jquerySelector.attr('data-vip-url');
-        ad.image = $jquerySelector.find('.image img').attr('src');
-        ad.title = $jquerySelector.find('a.title').text().trim();
-        ad.description = $jquerySelector.find('.description').text().trim();
-        ad.location = $jquerySelector.find('.location').text().trim();
-        ad.price = $jquerySelector.find('.price').text().trim();
-        ad.datePosted = Ad.determineDatePosted($jquerySelector.find('.date-posted').text().trim());
-
-        return ad;
-    }
-
-    static determineDatePosted(datePostedText) {
-        if (!datePostedText) {
-            return null;
-        }
-
-        var relativeTimeRegex = /(\d+) (hour|minute|second)s? ago/;
-        var match = relativeTimeRegex.exec(datePostedText);
-        if (match != null) {
-            return moment().subtract(match[1], match[2]);
-        }
-
-        if (datePostedText === 'Yesterday') {
-            return moment().subtract(1, 'day');
-        }
-
-        var parsedDateString = moment(datePostedText, 'DD-MM-YYYY');
-        if (parsedDateString.isValid()) {
-            return parsedDateString;
-        }
-
-        console.error("Unexpected date posted text:" + datePostedText);
-        return moment();
-    }
-
-    queryIsBusinessAd() {
-        return new RSVP.Promise((resolve, reject) => {
-            request(this.url, (error, response, html) => {
-                const $ = cheerio.load(html);
-                const adProfile = $('#R2SProfile').text();
-                this.isBusiness = adProfile.includes('Business') || adProfile.includes('Retail');
-                resolve(this);
-            });
-        });
-    }
-
-    isEqual(ad) {
-        return ad.url === this.url;
-    }
-
-    isInList(ads) {
-        return _.some(ads, ad => this.isEqual(ad));
-    }
-
-    toHtml() {
-        return `<tr><td><a href="${this.url}">${this.title}</a> Price: ${this.price}</td></tr>` +
-        `<tr><td>${this.location}</td></tr>` +
-        `<tr><td>${this.description}</td></tr>` +
-        `<tr><td><a href="${this.url}"><img src="${this.image}"/></a></td></tr>` +
-        `<tr><td>&nbsp;</td></tr>`;
-    }
-}
-
 function loadProcessedAds() {
     try {
         return require("./processedAds.json")
-    } catch(e) {
+    } catch (e) {
         return [];
     }
 }
 
 var failedToSave = false;
+
 function saveProcessedAds(processedAds) {
     try {
-        fs.writeFile( "processedAds.json", JSON.stringify(processedAds, null, '\t'), "utf8");
+        fs.writeFile("processedAds.json", JSON.stringify(processedAds, null, '\t'), "utf8");
     } catch (e) {
         if (!failedToSave) {
             console.error("unable to save processed ads")
@@ -227,7 +151,7 @@ function formatAds(ads) {
     const adsTableRows = ads.map(ad => ad.toHtml());
 
     return `<h1>${adsFoundMessage}</h1>` +
-    `<table>${adsTableRows}</table>`;
+        `<table>${adsTableRows}</table>`;
 }
 
 const cronRule = `*/${config.minutesBetweenCheck} * * * *`;
